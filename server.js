38482ba74f98dbe5a1fcd3f469d81595d9145a7e
Thomas Ravetto <travetto18@gmail.com>
2023-08-31 19:26:42 +0200

    fixed security issues

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express, { json } from "express";
+import dotenv from 'dotenv';
 import knex from "knex";
 import cors from "cors";
 import bcrypt from "bcrypt";
@@ -6,43 +7,48 @@ import fetch from "node-fetch";
 import { hashPassword, sendError, loginUser, registerUser, parseCollectionData, parseItemData, addCollectionToWatchlist, removeCollectionFromWatchlist, parseWatchlistItems, calculateOwners, isCollectionInWatchlist } from "./helpers.js";
 const PORT = 3500;
 
-const watchlist = [
-  {
-    image_url:"https://i.seadn.io/gae/_zidXBb2QsMBD6OYdjED63tczeXDUr1ah7zvhSSLHQjU4BF-H-lUexkLJ76_ahmbkkItEiH738jVPG88DOFVdt4GX377cvNNgCyzFT4?w=500&auto=format",
-    collection_name:"Alien Frens",
-    slug:"alienfrensnft",
-    floor_price:"0.089",
-    owners:"6124"
-  },
-  {
-    image_url:"https://i.seadn.io/gae/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB?w=500&auto=format",
-    collection_name:"Bored Ape Yacht Club",
-    slug:"boredapeyachtclub",
-    floor_price:"33.92",
-    owners:"5581"
-  },
-  {
-    image_url:"https://i.seadn.io/gae/7B0qai02OdHA8P_EOVK672qUliyjQdQDGNrACxs7WnTgZAkJa_wWURnIFKeOh5VTf8cfTqW3wQpozGedaC9mteKphEOtztls02RlWQ?w=500&auto=format",
-    collection_name:"Doodles",
-    slug:"doodles-official",
-    floor_price:"1.48",
-    owners:"5548"
-  },
-  {
-    image_url:"https://i.seadn.io/gae/seJEwLWJP3RAXrxboeG11qbc_MYrxwVrsxGH0s0qxvF68hefOjf5qrPSKkIknUTYzfvinOUPWbYBdM8VEtGEE980Qv2ti_GGd86OWQ?w=500&auto=format",
-    collection_name:"Deadfellaz",
-    slug:"deadfellaz",
-    floor_price:"0.17",
-    owners:"6522"
-  },
-  {
-    image_url:"https://i.seadn.io/gcs/files/b1c9ed2e584b4f6e418bf1ca15311844.jpg?w=500&auto=format",
-    collection_name:"Opepen",
-    slug:"opepen-edition",
-    floor_price:"0.69",
-    owners:"4191"
-  },
-]
+dotenv.config();
+
+
+const API_KEY = process.env.API_KEY
+
+// const watchlist = [
+//   {
+//     image_url:"https://i.seadn.io/gae/_zidXBb2QsMBD6OYdjED63tczeXDUr1ah7zvhSSLHQjU4BF-H-lUexkLJ76_ahmbkkItEiH738jVPG88DOFVdt4GX377cvNNgCyzFT4?w=500&auto=format",
+//     collection_name:"Alien Frens",
+//     slug:"alienfrensnft",
+//     floor_price:"0.089",
+//     owners:"6124"
+//   },
+//   {
+//     image_url:"https://i.seadn.io/gae/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB?w=500&auto=format",
+//     collection_name:"Bored Ape Yacht Club",
+//     slug:"boredapeyachtclub",
+//     floor_price:"33.92",
+//     owners:"5581"
+//   },
+//   {
+//     image_url:"https://i.seadn.io/gae/7B0qai02OdHA8P_EOVK672qUliyjQdQDGNrACxs7WnTgZAkJa_wWURnIFKeOh5VTf8cfTqW3wQpozGedaC9mteKphEOtztls02RlWQ?w=500&auto=format",
+//     collection_name:"Doodles",
+//     slug:"doodles-official",
+//     floor_price:"1.48",
+//     owners:"5548"
+//   },
+//   {
+//     image_url:"https://i.seadn.io/gae/seJEwLWJP3RAXrxboeG11qbc_MYrxwVrsxGH0s0qxvF68hefOjf5qrPSKkIknUTYzfvinOUPWbYBdM8VEtGEE980Qv2ti_GGd86OWQ?w=500&auto=format",
+//     collection_name:"Deadfellaz",
+//     slug:"deadfellaz",
+//     floor_price:"0.17",
+//     owners:"6522"
+//   },
+//   {
+//     image_url:"https://i.seadn.io/gcs/files/b1c9ed2e584b4f6e418bf1ca15311844.jpg?w=500&auto=format",
+//     collection_name:"Opepen",
+//     slug:"opepen-edition",
+//     floor_price:"0.69",
+//     owners:"4191"
+//   },
+// ]
 
 // const collections = [
 //   {
@@ -105,9 +111,9 @@ const db = knex({
     connection: {
       host : '127.0.0.1',
       port : 5432,
-      user : 'thomasravetto',
+      user : process.env.USER,
       password : '',
-      database : 'nftanalyser'
+      database : process.env.DATABASE
     }
   });
 
@@ -141,7 +147,6 @@ app.post("/register", (req, res) => {
 app.get("/collection/:collectionSlug/:startToken?", (req, res) => {
   const {collectionSlug} = req.params;
   let {startToken} = req.params;
-  const API_KEY = "GPgRRtpefy6USm7WE1Zfa1B0o0wO2y-7";
   const withMetadata = true;
   const NFTlimit = 20;
 
@@ -168,7 +173,6 @@ app.get("/collection/:collectionSlug/:startToken?", (req, res) => {
 
 app.get("/:collectionHash/:tokenId", (req, res) => {
   const {collectionHash, tokenId} = req.params;
-  const API_KEY = "GPgRRtpefy6USm7WE1Zfa1B0o0wO2y-7";
   const tokenType = "ERC721";
 
   fetch('https://eth-mainnet.g.alchemy.com/nft/v2/'+ API_KEY +'/getNFTMetadata?contractAddress=' + collectionHash + '&tokenId=' + tokenId + '&tokenType='+ tokenType +'&refreshCache=false')
@@ -199,7 +203,6 @@ app.delete("/remove-from-watchlist", (req, res) => {
 })
 
 app.post("/fetch-watchlist", (req, res) => {
-  const API_KEY = "GPgRRtpefy6USm7WE1Zfa1B0o0wO2y-7";
   const {username} = req.body;
   db.select("collections")
   .from("watchlist")
@@ -215,7 +218,6 @@ app.post("/fetch-watchlist", (req, res) => {
 })
 
 app.post("/is-collection-in-watchlist", async (req, res) => {
-  const API_KEY = "GPgRRtpefy6USm7WE1Zfa1B0o0wO2y-7";
   const {username, collection_address} = req.body;
   const collectionInWatchlist = await isCollectionInWatchlist(collection_address, username, db, res)
   res.json(collectionInWatchlist)
